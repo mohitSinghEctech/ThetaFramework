a8a681bc70379088b18ccc5c849094c4d85c14cd
Mohit Kumar Singh <mohit.kumar@nickelfox.co>
2022-02-23 12:52:40 +0530

    Initial Commit

---
+++ ThetaFramework/ThetaFramework.h
@@ -0,0 +1,18 @@
+//
+//  ThetaFramework.h
+//  ThetaFramework
+//
+//  Created by Mohit Kumar Singh on 23/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ThetaFramework.
+FOUNDATION_EXPORT double ThetaFrameworkVersionNumber;
+
+//! Project version string for ThetaFramework.
+FOUNDATION_EXPORT const unsigned char ThetaFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThetaFramework/PublicHeader.h>
+
+
