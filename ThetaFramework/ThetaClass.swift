851260ddc1cbc91c6db9668955c8a0ef39e76b04
Mohit Kumar Singh <mohit.kumar@nickelfox.co>
2022-02-23 12:57:26 +0530

    initial commit

---
+++ ThetaFramework/ThetaClass.swift
@@ -0,0 +1,16 @@
+//
+//  ThetaClass.swift
+//  ThetaFramework
+//
+//  Created by Mohit Kumar Singh on 23/02/22.
+//
+
+import Foundation
+
+public class ThetaClass {
+    init() {}
+    
+    public func getLogs() {
+        print("Implemented theta framework")
+    }
+}
